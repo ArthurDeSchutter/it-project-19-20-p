eab903262a7be2d708acd81b89cf850b45c5ac30
Oussama Ayadi <s115785@ap.be>
2020-02-21 19:58:19 +0100

    Dit is de frontend die ik momenteel heb

---
+++ it-project/mapscript.js
@@ -0,0 +1,20 @@
+console.log("test")
+
+var map = L.map('mapid').setView([51.2171918, 4.4212529], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.2171918, 4.4212529]).addTo(map)
+    .bindPopup('antwerpen')
+    .openPopup();
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
